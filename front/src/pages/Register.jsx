b2f3aa1b9235c8b27e7c8256fc532e73ec7e9d1d
idembele70 <idembele70@gmail.com>
2022-04-08 16:05:21 +0200

    [front] [register] fixing error message display before registering

--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -1,12 +1,11 @@
 import Navbar from "components/tools/Navbar"
+import { SendEmail } from "components/tools/utils"
 import React, { useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { useDispatch, useSelector } from "react-redux"
-import { useHistory, useLocation, Link } from "react-router-dom"
-import { mobile } from "responsive"
+import { Link, useHistory, useLocation } from "react-router-dom"
+import { mobile, tablet } from "responsive"
 import styled from "styled-components"
-import Emailjs from "@emailjs/browser"
-import { SendEmail } from "components/tools/utils"
 import { register } from "../redux/apiCalls"
 
 const Container = styled.div`
@@ -47,12 +46,18 @@ const Title = styled.h1`
 const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
+  ${tablet({ flexDirection: "column" })}
 `
-const Input = styled.input`
+const FormRow = styled.div`
   flex: 1;
   min-width: 40%;
   margin: 20px 10px 0px 0px;
+  display: flex;
+`
+const Input = styled.input`
+  flex: 1;
   padding: 10px;
+  width: 100%;
 `
 const Agreement = styled.div`
   font-size: 12px;
@@ -146,7 +151,6 @@ function Register() {
     password: "",
     confirmPassword: ""
   })
-  const [consent, setConsent] = useState(false)
   const [error, setError] = useState("")
   const [isTouched, setIsTouched] = useState(false)
   const handleUpdate = (e) => {
@@ -159,12 +163,13 @@ function Register() {
   const [loading, setLoading] = useState(false)
   const dispatch = useDispatch()
   const handleRegister = async (e) => {
+    e.preventDefault()
     setLoading(true)
     setIsTouched(true)
-    e.preventDefault()
     const { password, confirmPassword, email, confirmEmail } = data
-    if (!consent || password !== confirmPassword || email !== confirmEmail) {
-      if (!consent) setError(t("signup.agreementError"))
+    if (password !== confirmPassword || email !== confirmEmail) {
+      if (email !== confirmEmail) setError(t("signup.email"))
+      else if (password !== confirmPassword) setError(t("signup.passwordError"))
       setLoading(false)
     } else {
       const { confirmPassword: ignoredPassword, ...others } = data
@@ -215,60 +220,69 @@ function Register() {
         <Wrapper>
           <Title>{t("signup.title")}</Title>
           <Form onSubmit={handleRegister}>
-            <Input
-              required
-              value={data.firstname}
-              onChange={handleUpdate}
-              name="firstname"
-              placeholder={t("signup.firstname")}
-            />
-            <Input
-              required
-              value={data.lastname}
-              onChange={handleUpdate}
-              name="lastname"
-              placeholder={t("signup.lastname")}
-            />
-            <Input
-              required
-              type="email"
-              value={data.email}
-              onChange={handleUpdate}
-              name="email"
-              placeholder={t("sign.email")}
-            />
-            <Input
-              required
-              type="email"
-              value={data.confirmEmail}
-              onChange={handleUpdate}
-              name="confirmEmail"
-              placeholder={t("signup.confirmEmail")}
-            />
-            <Input
-              required
-              type="password"
-              value={data.password}
-              onChange={handleUpdate}
-              name="password"
-              placeholder={t("sign.password")}
-            />
-            <Input
-              required
-              type="password"
-              value={data.confirmPassword}
-              onChange={handleUpdate}
-              name="confirmPassword"
-              placeholder={t("signup.confirmPassword")}
-            />
+            <FormRow>
+              <Input
+                required
+                value={data.firstname}
+                onChange={handleUpdate}
+                name="firstname"
+                placeholder={t("signup.firstname")}
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                required
+                value={data.lastname}
+                onChange={handleUpdate}
+                name="lastname"
+                placeholder={t("signup.lastname")}
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                required
+                type="email"
+                value={data.email}
+                onChange={handleUpdate}
+                name="email"
+                placeholder={t("sign.email")}
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                required
+                type="email"
+                value={data.confirmEmail}
+                onChange={handleUpdate}
+                name="confirmEmail"
+                placeholder={t("signup.confirmEmail")}
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                autoComplete="new-password"
+                required
+                type="password"
+                value={data.password}
+                onChange={handleUpdate}
+                name="password"
+                placeholder={t("sign.password")}
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                autoComplete="new-password"
+                required
+                type="password"
+                value={data.confirmPassword}
+                onChange={handleUpdate}
+                name="confirmPassword"
+                placeholder={t("signup.confirmPassword")}
+              />
+            </FormRow>
             <Error opacity={error ? 1 : 0}>{error}</Error>
             <Agreement>
-              <InputRadio
-                type="checkbox"
-                checked={consent}
-                required
-                onChange={() => setConsent(!consent)}
-              />
+              <InputRadio type="checkbox" required />
               <InlineText>{t("signup.agreement")}</InlineText>
               <StyledLink target="_blank" to="/cgv">
                 {t("signup.privacyPolicy")}
